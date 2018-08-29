b971f950a8bcb0d90225f1bba96f3be35198e545
David Sally <David.X.Sally.-ND@disney.com>
2018-08-29 16:30:25 -0700

    (add) skeleton text box

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TextInput = () => {
+  return (
+    <div className="container">
+      <input className="container" />
+    </div>
+  );
+};
+
+export default TextInput;
