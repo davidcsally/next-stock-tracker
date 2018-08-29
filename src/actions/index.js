6a04094f0db99d47f057144cff7e82cc33ac02e4
David Sally <David.X.Sally.-ND@disney.com>
2018-08-29 16:25:28 -0700

    (add) first round of actions

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+// fetch
+export const FETCH_DATA = 'FETCH_DATA';
+
+// watching
+export const WATCH_STOCK = 'WATCH_STOCK';
+
+// add
+export const ADD_STOCK = 'ADD_STOCK';
+export const ADD_STOCK_SUCCESS = 'ADD_STOCK_SUCCESS';
+export const ADD_STOCK_FAILURE = 'ADD_STOCK_FAILURE';
+
+// remove
+export const REMOVE_STOCK = 'REMOVE_STOCK';
+
+// update
+export const UPDATE_STOCKS = 'UPDATE_STOCKS';
+export const UPDATE_STOCKS_SUCCESS = 'UPDATE_STOCKS_SUCCESS';
+
+// trades
+export const ADD_TRADE = 'ADD_TRADE';
+export const ADD_TRADE_SUCCESS = 'ADD_TRADE_SUCCESS';
