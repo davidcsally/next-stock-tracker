ee91861eb14aa673187748d4cce38f8f8d23c181
David Sally <David.X.Sally.-ND@disney.com>
2018-09-05 09:59:49 -0700

    (add) portfolio to initial state for testing

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -2,9 +2,29 @@ export default {
   stocks: [
     {
       ticker: 'BIO',
+      // price: 100,
+      // volume: 100,
     },
     {
       ticker: 'TSLA',
     },
   ],
+  portfolio: {
+    BIO: [
+      {
+        purchasePrice: 100,
+        shares: 10,
+      },
+      {
+        purchasePrice: 110,
+        shares: 12,
+      },
+    ],
+    TSLA: [
+      {
+        purchasePrice: 40,
+        shares: 20,
+      },
+    ],
+  },
 };
