c2c59e482794f9dbb4d4953bdc0a4443c97e0044
David Sally <David.X.Sally.-ND@disney.com>
2018-09-10 08:58:30 -0700

    (update) add undef / empty check, use grid layout

--- src/components/WatchList/WatchList.jsx
+++ src/components/WatchList/WatchList.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './WatchList.scss';
 
-// function generateList(data) {
-
-// }
-
 // list
 const ListItem = ({ ticker, price, volume, removeStock }) => (
   <li styleName="list-item">
     <p styleName="ticker">{ticker}</p>
-    <p styleName="price">{price}</p>
+    <p styleName="price">{(typeof price === 'undefined' || price === '') ? '---' : Number(price).toFixed(2)}</p>
     <button
       type="button"
       onClick={() => removeStock(ticker)}
@@ -21,26 +17,23 @@ const ListItem = ({ ticker, price, volume, removeStock }) => (
 
 
 // show list of watched stocks
-const WatchList = ({ actions, data }) => {
-  // console.log('data', data);
-  return (
-    <div styleName="container">
-      <button type="button" styleName="std-button" onClick={actions.fetchData}>
-        Fetch data
-      </button>
-      <ol styleName="list">
-        {data.map(d => (
-          <ListItem
-            ticker={d.ticker}
-            price={d.price}
-            volume={d.volume}
-            removeStock={actions.removeStock}
-            key={d.ticker}
-          />
-        ))}
-      </ol>
-    </div>
-  );
-};
+const WatchList = ({ actions, data }) => (
+  <div styleName="container">
+    <button type="button" styleName="std-button" onClick={actions.fetchData}>
+      Fetch data
+    </button>
+    <ol styleName="list">
+      {data.map(d => (
+        <ListItem
+          ticker={d.ticker}
+          price={d.price}
+          volume={d.volume}
+          removeStock={actions.removeStock}
+          key={d.ticker}
+        />
+      ))}
+    </ol>
+  </div>
+);
 
 export default WatchList;
