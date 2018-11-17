97d1c560d37995372fffeead0284ed9db4798436
David Sally <davidcsally@gmail.com>
2018-11-16 23:01:26 -0800

    (update) remove ListItem, update styles

--- src/components/TickerItem/TickerItem.jsx
+++ src/components/TickerItem/TickerItem.jsx
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import Modal from '../Modal/Modal';
+import ListItem from '../ListItem/ListItem';
 import './TickerItem.scss';
-
-/** Used in TickerItem */
-const ListItem = ({ purchasePrice, shares, actions, index, ticker }) => (
-  <li styleName="list-item">
-    <p>purchase price: {purchasePrice}</p>
-    <p>shares: {shares}</p>
-    <button
-      styleName="button-remove"
-      type="button"
-      onClick={() => actions.removeBuy({ ticker, index })}
-    >
-      <span role="img" aria-label="remove">🚫</span>
-    </button>
-  </li>
-);
-
-ListItem.propTypes = {
-  ticker: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
-  shares: PropTypes.number.isRequired,
-  purchasePrice: PropTypes.number.isRequired,
-};
 
 /** Used in PortfolioList */
 class TickerItem extends Component {
+  static propTypes = {
+    buys: PropTypes.array,
+    ticker: PropTypes.string,
+    actions: PropTypes.any,
+  };
+
   constructor() {
     super();
-    this.state = { isModalOpen: false };
+    this.state = {
+      isModalOpen: false,
+      isListShown: true,
+    };
   }
 
   toggleTradesModal = () => {
-    this.setState({ isModalOpen: !this.state.isModalOpen });
+    const { isModalOpen } = this.state;
+    this.setState({ isModalOpen: !isModalOpen });
   };
+
+  toggleList = () => {
+    const { isListShown } = this.state;
+    this.setState({ isListShown: !isListShown });
+  }
 
   render() {
     const { buys, ticker, actions } = this.props;
+    const { isModalOpen, isListShown } = this.state;
 
     // calculate the totals for price and shares
     const totalShares = buys.reduce((prev, current) => prev + current.shares, 0);
@@ -45,36 +41,38 @@ class TickerItem extends Component {
 
     return (
       <li styleName="ticker-item">
-        <div styleName="ticker">
-          <p styleName="ticker">{ticker}</p>
-          <p>total shares: {totalShares}</p>
+        <div styleName="ticker" onClick={this.toggleList} role="button" tabIndex="0">
+          <p>{ticker}</p>
+          <p>shares: {totalShares}</p>
           <p styleName="col-value">${Number(totalValue).toFixed(2)}</p>
         </div>
-        <ol styleName="buys-list">
-          {buys.map((t, i) => (
-            <ListItem
-              actions={actions}
-              key={t.shares + t.purchasePrice}
-              purchasePrice={t.purchasePrice}
-              shares={t.shares}
-              index={i}
-              ticker={ticker}
-            />
-          ))}
-          <li styleName="cell-add-trade">
-            <button
-              type="button"
-              onClick={this.toggleTradesModal}
-            >
-              Add trade
-            </button>
-            {this.state.isModalOpen &&
-              <Modal onCloseRequest={this.toggleTradesModal}>
-                <AddTradesModal />
-              </Modal>
-            }
-          </li>
-        </ol>
+        {isListShown && (
+          <ol styleName="buys-list">
+            {buys.map((t, i) => (
+              <ListItem
+                actions={actions}
+                key={t.shares + t.purchasePrice}
+                purchasePrice={t.purchasePrice}
+                shares={t.shares}
+                index={i}
+                ticker={ticker}
+              />
+            ))}
+            <li styleName="cell-add-trade">
+              <button
+                type="button"
+                onClick={this.toggleTradesModal}
+              >
+                Add trade
+              </button>
+              {isModalOpen && (
+                <Modal onCloseRequest={this.toggleTradesModal}>
+                  <AddTradesModal />
+                </Modal>)
+              }
+            </li>
+          </ol>)
+        }
       </li>
     );
   }
