509ddff7eae3d89e27954a89869a34593afeefe2
David Sally <davidcsally@gmail.com>
2018-11-18 19:33:34 -0800

    (add) time series daily url constructor

--- src/api/urls.js
+++ src/api/urls.js
@@ -7,23 +7,36 @@ const { API_KEY } = publicRuntimeConfig;
 const RequestType = {
   BATCH_STOCK_QUOTES: 'BATCH_STOCK_QUOTES',
   GLOBAL_QUOTE: 'GLOBAL_QUOTE',
+  TIME_SERIES_DAILY: 'TIME_SERIES_DAILY',
 };
 
-const { BATCH_STOCK_QUOTES, GLOBAL_QUOTE } = RequestType;
+const {
+  BATCH_STOCK_QUOTES,
+  GLOBAL_QUOTE,
+  TIME_SERIES_DAILY,
+} = RequestType;
+
 const BASE_URL = 'https://www.alphavantage.co';
 
 const AlphaVantageUrl = `${BASE_URL}/query?function=`;
 
 /**
- * rename this - for dow jones index
+ * Global Quote - for dow jones
  * @param {string} symbol - single stock ticker string
  */
 export const globalQuote = symbol =>
   `${AlphaVantageUrl}${GLOBAL_QUOTE}&symbol=${symbol}&apikey=${API_KEY}`;
 
 /**
- * main url
+ * Batch Stock Request
  * @param {Array} symbols - array of stock tickers
  */
 export const updateUrl = symbols =>
   `${AlphaVantageUrl}${BATCH_STOCK_QUOTES}&symbols=${symbols.toString()}&apikey=${API_KEY}`;
+
+/**
+ * Time Series - Daily
+ * @param {string} ticker
+ */
+export const fetchTimeSeriesDaily = symbol =>
+  `${AlphaVantageUrl}${TIME_SERIES_DAILY}&symbol=${symbol}&apikey=${API_KEY}`;
