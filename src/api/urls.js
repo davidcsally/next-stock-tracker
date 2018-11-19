5231783cdc294e6a9d6fb6b5f147f8c9f8a836c3
David Sally <davidcsally@gmail.com>
2018-11-18 19:26:15 -0800

    (refactor) urls and add method for grabbing dow jones

--- src/api/urls.js
+++ src/api/urls.js
@@ -6,12 +6,24 @@ const { API_KEY } = publicRuntimeConfig;
 
 const RequestType = {
   BATCH_STOCK_QUOTES: 'BATCH_STOCK_QUOTES',
+  GLOBAL_QUOTE: 'GLOBAL_QUOTE',
 };
 
-const { BATCH_STOCK_QUOTES } = RequestType;
+const { BATCH_STOCK_QUOTES, GLOBAL_QUOTE } = RequestType;
 const BASE_URL = 'https://www.alphavantage.co';
-const AlphaVantageUrl = `${BASE_URL}/query?function=${BATCH_STOCK_QUOTES}`;
 
+const AlphaVantageUrl = `${BASE_URL}/query?function=`;
 
+/**
+ * rename this - for dow jones index
+ * @param {string} symbol - single stock ticker string
+ */
+export const globalQuote = symbol =>
+  `${AlphaVantageUrl}${GLOBAL_QUOTE}&symbol=${symbol}&apikey=${API_KEY}`;
+
+/**
+ * main url
+ * @param {Array} symbols - array of stock tickers
+ */
 export const updateUrl = symbols =>
-  `${AlphaVantageUrl}&symbols=${symbols.toString()}&apikey=${API_KEY}`;
+  `${AlphaVantageUrl}${BATCH_STOCK_QUOTES}&symbols=${symbols.toString()}&apikey=${API_KEY}`;
