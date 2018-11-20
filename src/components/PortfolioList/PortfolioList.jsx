971b4862f6fd9c7a74e7595a196e6e06081fb3a6
David Sally <davidcsally@gmail.com>
2018-11-20 14:39:40 -0800

    (update) add a currency formatter and display total value

--- src/components/PortfolioList/PortfolioList.jsx
+++ src/components/PortfolioList/PortfolioList.jsx
@@ -2,27 +2,46 @@ import React from 'react';
 import TickerItem from '../TickerItem/TickerItem';
 import './PortfolioList.scss';
 
+const currencyFormatter = (value) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  });
+
+  return formatter.format(value);
+};
+
 /**
  * show list of watched stocks
  *
  * Used in PortfolioListContainer
  */
-const PortfolioList = ({ data, actions }) => (
-  <div styleName="container">
-    <button styleName="std-button" onClick={actions.fetchData}>
-      Fetch Data
-    </button>
-    <ol styleName="parent-list">
-      {data && Object.keys(data).map(t => (
-        <TickerItem
-          key={t}
-          ticker={t}
-          buys={data[t]}
-          actions={actions}
-        />
-      ))}
-    </ol>
-  </div>
-);
+const PortfolioList = ({ data, actions, totalValue }) => {
+  return (
+    <div styleName="container">
+      <h2>Your Portfolio</h2>
+      <h1>{currencyFormatter(totalValue)}</h1>
+      <button
+        type="button"
+        styleName="std-button"
+        onClick={actions.fetchData}
+      >
+        Fetch Data
+      </button>
+      <ol styleName="parent-list">
+        {/* <p>%holdings</p> */}
+        {data && Object.keys(data).map(t => (
+          <TickerItem
+            key={t}
+            ticker={t}
+            buys={data[t]}
+            actions={actions}
+          />
+        ))}
+      </ol>
+    </div>
+  );
+};
 
 export default PortfolioList;
